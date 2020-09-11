1e35542848a8e93ff3f049d67243292682093340
Bartosz Janusz <bartosz.yan@gmail.com>
2020-09-11 16:57:15 +0200

    mobile home page

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -1,22 +1,24 @@
 let hamburger = document.querySelector('.nav-hamburger');
 let bar = hamburger.getElementsByTagName("span");
+let mobileMenu = document.getElementById("mobileMenu");
+
+let active = false;
 
 const overlay = () => {
 
+  if (active === false) {
+    mobileMenu.style.display = "block";
     bar[0].style.transform = "translateY(12px) rotate(135deg)";
     bar[1].style.transform = "scale(0)";
     bar[2].style.transform = "translateY(-12px) rotate(-135deg)";
+    active = true;
+  } else {
+    mobileMenu.style.display = "none";
+    bar[0].style.transform = "none";
+    bar[1].style.transform = "scale(1)";
+    bar[2].style.transform = "none";
+    active = false;
+  }
 }
 
-/* Open
-function openNav() {
-    document.getElementById("myNav").style.display = "block";
-  } */
-  
-  /* Close 
-  function closeNav() {
-    document.getElementById("myNav").style.display = "none";
-  }*/
-
-  hamburger.addEventListener("click", overlay);
-
+hamburger.addEventListener("click", overlay);
