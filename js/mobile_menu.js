2e6cb9903cb7bb114bb1caed0389922b63b77359
Bartosz Janusz <bartosz.yan@gmail.com>
2020-09-29 14:34:36 +0200

    nav menu js & css changes

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -1,6 +1,7 @@
 let hamburger = document.querySelector('.nav_hamburger');
 let bar = hamburger.getElementsByTagName("span");
 let mobileMenu = document.getElementById("mobileMenu");
+let overlayLink = document.querySelectorAll('.nav_overlay-content--link');
 
 let active = false;
 
@@ -8,17 +9,26 @@ const overlay = () => {
 
   if (active === false) {
     mobileMenu.style.display = "block";
-    bar[0].style.transform = "translateY(12px) rotate(135deg)";
+    bar[0].style.cssText = "background-color: #fffef9; transform: translateY(12px) rotate(135deg)";
     bar[1].style.transform = "scale(0)";
-    bar[2].style.transform = "translateY(-12px) rotate(-135deg)";
+    bar[2].style.cssText = "background-color: #fffef9; transform: translateY(-12px) rotate(-135deg)";
     active = true;
   } else {
     mobileMenu.style.display = "none";
-    bar[0].style.transform = "none";
+    bar[0].style.cssText = "background-color: #000; transform: none";
     bar[1].style.transform = "scale(1)";
-    bar[2].style.transform = "none";
+    bar[2].style.cssText = "background-color: #000; transform: none";
     active = false;
   }
 }
+
+overlayLink.forEach(item => {
+  item.addEventListener('click', event => {
+    mobileMenu.style.display = "none";
+    bar[0].style.cssText = "background-color: #000; transform: none";
+    bar[1].style.transform = "scale(1)";
+    bar[2].style.cssText = "background-color: #000; transform: none";
+  })
+})
 
 hamburger.addEventListener("click", overlay);
