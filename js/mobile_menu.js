ade524c8f739d08ff7d31a3eb5ceffa7a7aca3ec
Bartosz Janusz <bartosz.yan@gmail.com>
2020-10-04 16:03:34 +0200

    index detail changes

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -1,8 +1,10 @@
+//hamburger and overlay variables
 let hamburger = document.querySelector('.nav_hamburger');
 let bar = hamburger.getElementsByTagName("span");
 let mobileMenu = document.getElementById("mobileMenu");
 let overlayLink = document.querySelectorAll('.nav_overlay-content--link');
 
+//Hamburger overlay functions
 let active = false;
 
 const overlay = () => {
@@ -20,7 +22,7 @@ const overlay = () => {
     bar[2].style.cssText = "background-color: #000; transform: none";
     active = false;
   }
-}
+};
 
 overlayLink.forEach(item => {
   item.addEventListener('click', event => {
@@ -30,6 +32,65 @@ overlayLink.forEach(item => {
     bar[2].style.cssText = "background-color: #000; transform: none";
     active = false;
   })
-})
+});
 
 hamburger.addEventListener("click", overlay);
+
+
+
+//Logo Spin variables
+let hoverArea = document.querySelector('.nav_logo');
+let logoTxt = document.querySelector('.nav_logo-txt');
+let flag = true;
+
+//Logo Spin animation on hover
+const rotate = () => {
+  logoTxt.classList.add("rotating");
+  flag = false;
+}
+
+const rotated = () => {
+  if (flag === false)
+    logoTxt.classList.remove("rotating");
+};
+
+hoverArea.addEventListener('mouseover', rotate);
+hoverArea.addEventListener('mouseout', rotated);
+
+
+
+
+/*Try with transform and SETINTERVAL - celar SETINTERRVAL
+//Logo Spin variables
+let hoverArea = document.querySelector('.nav_logo');
+let logoTxt = document.querySelector('.nav_logo-txt');
+let flag = true;
+
+//Logo Spin animation on hover
+const rotate = () => {
+  if (flag === true){
+  logoTxt.style.transform = "rotate(-360deg)";
+}
+};
+
+hoverArea.addEventListener('mouseover', rotate);
+*/
+
+/*//Logo Spin variables
+let hoverArea = document.querySelector('.nav_logo');
+let logoTxt = document.querySelector('.nav_logo-txt');
+let flag = false;
+
+//Logo Spin animation on hover
+let rotate = () => {
+  if (flag === false) {
+    logoTxt.classList.add("rotating");
+    flag = true;
+  }
+};
+
+let rotated = () => flag = false;
+
+
+hoverArea.addEventListener('mouseover', rotate);
+hoverArea.addEventListener('mouseout', rotated);*/
