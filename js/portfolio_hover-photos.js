afd80c626ea53c15d3d78d8b3a413b49dbce7924
Bartosz Janusz <bartosz.yan@gmail.com>
2020-10-08 16:30:40 +0200

    portfolio list js changes

--- js/portfolio_hover-photos.js
+++ js/portfolio_hover-photos.js
@@ -1,12 +1,35 @@
 let link = document.querySelectorAll('.portfolio_list-link--item');
 let photosBox = document.querySelectorAll('.portfolio_photos');
 
-const show = () => {
+
+var i = 0;
+
+for (i = 0; i < link.length; i++) {
+
+  link[i].addEventListener('mouseover',  e => {
+    photosBox[0].style.visibility = "visible";
     photosBox[0].style.opacity = "1";
+    photosBox[0].style.zIndex = "-100";
+  });
+
+  link[i].addEventListener('mouseout', e => {
+    photosBox[0].style.visibility = "hidden";
+    photosBox[0].style.opacity = "0";
+    photosBox[0].style.zIndex = "-100";
+  });
+
+}
+
+/*
+const show = () => {
+    photosBox[0].style.visibility = "visible";
+    photosBox[0].style.opacity = "1";
+    photosBox[0].style.zIndex = "-100";
 }
 const hide = () => {
-    photosBox[0].style.opacity = "0";
-
+  photosBox[0].style.visibility = "hidden";
+  photosBox[0].style.opacity = "0";
+  photosBox[0].style.zIndex = "-100";
 }
 
 link[0].addEventListener('mouseover', show)
