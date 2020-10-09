8c28517a1388053b7a474fc4fa0ebb071fc3e90d
Bartosz Janusz <bartosz.yan@gmail.com>
2020-10-09 15:54:08 +0200

    portfolio list html i css structure

--- js/portfolio_hover-photos.js
+++ js/portfolio_hover-photos.js
@@ -2,20 +2,97 @@ let link = document.querySelectorAll('.portfolio_list-link--item');
 let photosBox = document.querySelectorAll('.portfolio_photos');
 
 
-var i = 0;
-
 for (i = 0; i < link.length; i++) {
 
-  link[i].addEventListener('mouseover',  e => {
-    photosBox[0].style.visibility = "visible";
-    photosBox[0].style.opacity = "1";
-    photosBox[0].style.zIndex = "-100";
+  link[i].addEventListener('mouseover', e => {
+    
+      photosBox[0].style.visibility = "visible";
+      photosBox[0].style.opacity = "1";
+      photosBox[0].style.zIndex = "-100";
+    
   });
 
   link[i].addEventListener('mouseout', e => {
-    photosBox[0].style.visibility = "hidden";
-    photosBox[0].style.opacity = "0";
-    photosBox[0].style.zIndex = "-100";
+    
+      photosBox[0].style.visibility = "hidden";
+      photosBox[0].style.opacity = "0";
+      photosBox[0].style.zIndex = "-100";
+    
+  });
+}
+
+
+
+
+
+
+
+
+
+/*
+for (var i = 0; i < array.length; i++) {
+  for (var j = 0; j < array[i].length; j++) {
+      newarray.push(array[i][j]);
+  }
+}*/
+
+/*
+
+function check(a){
+  for(var i = 0; i < link.length; i++){
+     if(link[i] === a) return true;
+  }return false;}
+
+  /*
+for(var i=0; i < link.length; i++) {
+  for(var j=0; j < photosBox.length; j++) {
+      if(link[i] === photosBox[j]) {
+        photosBox[j].style.visibility = "visible";
+        photosBox[j].style.opacity = "1";
+        photosBox[j].style.zIndex = "-100";
+       }
+  }
+};
+/*
+for (i = 0; i < link.length; i++) {
+
+  link[i].addEventListener('mouseover', e => {
+     
+      photosBox[j].style.visibility = "visible";
+      photosBox[j].style.opacity = "1";
+      photosBox[j].style.zIndex = "-100";
+    
+  
+  });
+
+  link[i].addEventListener('mouseout', e => {
+    
+      photosBox[j].style.visibility = "hidden";
+      photosBox[j].style.opacity = "0";
+      photosBox[j].style.zIndex = "-100";
+    
+  });
+
+}
+
+/*
+for (i = 0; i < link.length; i++) {
+
+  link[i].addEventListener('mouseover', e => {
+    for (var j = 0; j < link[i].length; j++){
+     
+      photosBox[j].style.visibility = "visible";
+      photosBox[j].style.opacity = "1";
+      photosBox[j].style.zIndex = "-100";
+    }
+  });
+
+  link[i].addEventListener('mouseout', e => {
+    
+      photosBox[0].style.visibility = "hidden";
+      photosBox[0].style.opacity = "0";
+      photosBox[0].style.zIndex = "-100";
+    
   });
 
 }
