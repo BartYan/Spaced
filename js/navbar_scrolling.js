571698db6cbf7462cf9159461f21f4a957493bfa
Bartosz Janusz <bartosz.yan@gmail.com>
2020-10-10 15:24:38 +0200

    home page portfolio & fonts changes

--- js/navbar_scrolling.js
+++ js/navbar_scrolling.js
@@ -8,13 +8,29 @@ window.onscroll = () => {
     document.querySelector('.nav').style.top = "0";
   } else {
     document.querySelector('.nav').style.top = "-100px";
+    //logo styles
     document.querySelector('.nav_logo').style.cssText = "width: 50px; padding: 10px 10px 0 0;";
     document.querySelector('.nav_logo-syg').style.cssText = "width: 50px;";
     document.querySelector('.nav_logo-txt').style.cssText = "width: 50px;";
+    //menu links styles
+    document.querySelectorAll('.nav_list-item').forEach(element => {
+      element.style.cssText = "padding: 12px 5px 20px 50px;";
+    });
+    document.querySelectorAll('.nav_list-item--link').forEach(element => {
+      element.style.cssText = "font-size: 10px;";
+    });
   } if (window.scrollY == 0 & window.innerWidth >= 1200) {
+    //logo styles
     document.querySelector('.nav_logo').style.cssText = "width: 105px; padding: 30px 30px 0 0;";
     document.querySelector('.nav_logo-syg').style.cssText = "width: 105;";
     document.querySelector('.nav_logo-txt').style.cssText = "width: 105;";
+    //menu links styles
+    document.querySelectorAll('.nav_list-item').forEach(element => {
+      element.style.cssText = "padding: 25px 5px 0 50px;";
+    });
+    document.querySelectorAll('.nav_list-item--link').forEach(element => {
+      element.style.cssText = "font-size: 12px;";
+    });
    }
   prevScrollpos = currentScrollPos;
 }
--- js/portfolio_hover-photos.js
+++ js/portfolio_hover-photos.js
@@ -1,123 +1,103 @@
 let link = document.querySelectorAll('.portfolio_list-link--item');
 let photosBox = document.querySelectorAll('.portfolio_photos');
 
+let i = 0;
 
+//It works, but it's not universal
+for (i = 0; i < link.length; i++) {
+  if (i == 0) {
+    link[0].addEventListener('mouseover', e => {
+      photosBox[0].style.display = "block";
+    });
+    link[0].addEventListener('mouseout', e => {
+      photosBox[0].style.display = "none";
+    });
+  } else if (i == 1) {
+    link[1].addEventListener('mouseover', e => {
+      photosBox[1].style.display = "block";
+    });
+    link[1].addEventListener('mouseout', e => {
+      photosBox[1].style.display = "none";
+    });
+  } else if (i == 2) {
+    link[2].addEventListener('mouseover', e => {
+      photosBox[2].style.display = "block";
+    });
+    link[2].addEventListener('mouseout', e => {
+      photosBox[2].style.display = "none";
+    });
+  } else if (i == 3) {
+    link[3].addEventListener('mouseover', e => {
+      photosBox[3].style.display = "block";
+    });
+    link[3].addEventListener('mouseout', e => {
+      photosBox[3].style.display = "none";
+    });
+  } else if (i == 4) {
+    link[4].addEventListener('mouseover', e => {
+      photosBox[4].style.display = "block";
+    });
+    link[4].addEventListener('mouseout', e => {
+      photosBox[4].style.display = "none";
+    });
+  } else if (i == 5) {
+    link[5].addEventListener('mouseover', e => {
+      photosBox[5].style.display = "block";
+    });
+    link[5].addEventListener('mouseout', e => {
+      photosBox[5].style.display = "none";
+    });
+  } else if (i == 6) {
+    link[6].addEventListener('mouseover', e => {
+      photosBox[6].style.display = "block";
+    });
+    link[6].addEventListener('mouseout', e => {
+      photosBox[6].style.display = "none";
+    });
+  } else if (i == 7) {
+    link[7].addEventListener('mouseover', e => {
+      photosBox[7].style.display = "block";
+    });
+    link[7].addEventListener('mouseout', e => {
+      photosBox[7].style.display = "none";
+    });
+  } else if (i == 8) {
+    link[8].addEventListener('mouseover', e => {
+      photosBox[8].style.display = "block";
+    });
+    link[8].addEventListener('mouseout', e => {
+      photosBox[8].style.display = "none";
+    });
+  } else if (i == 9) {
+    link[9].addEventListener('mouseover', e => {
+      photosBox[9].style.display = "block";
+    });
+    link[9].addEventListener('mouseout', e => {
+      photosBox[9].style.display = "none";
+    });
+  } 
+}
+
+/*
 for (i = 0; i < link.length; i++) {
 
   link[i].addEventListener('mouseover', e => {
     
-      photosBox[0].style.visibility = "visible";
-      photosBox[0].style.opacity = "1";
-      photosBox[0].style.zIndex = "-100";
+      photosBox[0].style.display = "block";
+ 
     
   });
 
   link[i].addEventListener('mouseout', e => {
-    
-      photosBox[0].style.visibility = "hidden";
-      photosBox[0].style.opacity = "0";
-      photosBox[0].style.zIndex = "-100";
-    
+
+      photosBox[0].style.display = "none";
+
   });
 }
+*/
 
 
 
 
 
 
-
-
-
-/*
-for (var i = 0; i < array.length; i++) {
-  for (var j = 0; j < array[i].length; j++) {
-      newarray.push(array[i][j]);
-  }
-}*/
-
-/*
-
-function check(a){
-  for(var i = 0; i < link.length; i++){
-     if(link[i] === a) return true;
-  }return false;}
-
-  /*
-for(var i=0; i < link.length; i++) {
-  for(var j=0; j < photosBox.length; j++) {
-      if(link[i] === photosBox[j]) {
-        photosBox[j].style.visibility = "visible";
-        photosBox[j].style.opacity = "1";
-        photosBox[j].style.zIndex = "-100";
-       }
-  }
-};
-/*
-for (i = 0; i < link.length; i++) {
-
-  link[i].addEventListener('mouseover', e => {
-     
-      photosBox[j].style.visibility = "visible";
-      photosBox[j].style.opacity = "1";
-      photosBox[j].style.zIndex = "-100";
-    
-  
-  });
-
-  link[i].addEventListener('mouseout', e => {
-    
-      photosBox[j].style.visibility = "hidden";
-      photosBox[j].style.opacity = "0";
-      photosBox[j].style.zIndex = "-100";
-    
-  });
-
-}
-
-/*
-for (i = 0; i < link.length; i++) {
-
-  link[i].addEventListener('mouseover', e => {
-    for (var j = 0; j < link[i].length; j++){
-     
-      photosBox[j].style.visibility = "visible";
-      photosBox[j].style.opacity = "1";
-      photosBox[j].style.zIndex = "-100";
-    }
-  });
-
-  link[i].addEventListener('mouseout', e => {
-    
-      photosBox[0].style.visibility = "hidden";
-      photosBox[0].style.opacity = "0";
-      photosBox[0].style.zIndex = "-100";
-    
-  });
-
-}
-
-/*
-const show = () => {
-    photosBox[0].style.visibility = "visible";
-    photosBox[0].style.opacity = "1";
-    photosBox[0].style.zIndex = "-100";
-}
-const hide = () => {
-  photosBox[0].style.visibility = "hidden";
-  photosBox[0].style.opacity = "0";
-  photosBox[0].style.zIndex = "-100";
-}
-
-link[0].addEventListener('mouseover', show)
-link[0].addEventListener('mouseout', hide);
-
-
-
-/*
-  link.forEach(item => {
-    item.addEventListener('mouseOver', event => {
-        photosBox[0].style.opacity = "1";
-    })
-  });
-  */
