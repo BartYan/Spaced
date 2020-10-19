6aa9c08d42f1727ac2240852228cbb61d9077e17
Bartosz Janusz <bartosz.yan@gmail.com>
2020-10-19 13:03:54 +0200

    nav new updates

--- js/navbar_scrolling.js
+++ js/navbar_scrolling.js
@@ -1,12 +1,9 @@
-/* Scrolls down = hide the navbar & logo is smaller when scrolling. 
-Scrolls up = show the navbar & logo is bigger*/
-
 let prevScrollpos = window.pageYOffset;
 window.onscroll = () => {
   let currentScrollPos = window.pageYOffset;
-  if ((prevScrollpos > currentScrollPos) || window.scrollY == 0) {
+  if (prevScrollpos > currentScrollPos) {
     document.querySelector('.nav').style.top = "0";
-  } else {
+   } else {
     document.querySelector('.nav').style.top = "-100px";
     //logo styles
     document.querySelector('.nav_logo').style.cssText = "width: 50px; padding: 10px 10px 0 0;";
@@ -19,9 +16,33 @@ window.onscroll = () => {
     document.querySelectorAll('.nav_list-item--link').forEach(element => {
       element.style.cssText = "font-size: 10px;";
     });
-  } 
-  
-  if (window.pageYOffset == 0 & window.innerWidth >= 1200) {
+  }
+
+  if (window.scrollY == 0) { 
+    //logo styles
+  document.querySelector('.nav_logo').style.cssText = "width: 105px; padding: 30px 30px 0 0;";
+  document.querySelector('.nav_logo-syg').style.cssText = "width: 105;";
+  document.querySelector('.nav_logo-txt').style.cssText = "width: 105;";
+  //menu links styles
+  document.querySelectorAll('.nav_list-item').forEach(element => {
+    element.style.cssText = "padding: 25px 5px 0 50px;";
+  });
+  document.querySelectorAll('.nav_list-item--link').forEach(element => {
+    element.style.cssText = "font-size: 12px;";
+  });
+ }
+  prevScrollpos = currentScrollPos;
+}
+
+/* Scrolls down = hide the navbar & logo is smaller when scrolling. 
+Scrolls up = show the navbar & logo is bigger*/
+/*
+let prevScrollpos = window.pageYOffset;
+window.onscroll = () => {
+  let currentScrollPos = window.pageYOffset;
+  if ((prevScrollpos > currentScrollPos) || window.scrollY == 0) {
+    document.querySelector('.nav').style.top = "0";
+  } else if (window.pageYOffset == 0 && window.innerWidth >= 1200) {
     document.querySelector('.nav').style.top = "0";
     //logo styles
     document.querySelector('.nav_logo').style.cssText = "width: 105px; padding: 30px 30px 0 0;";
@@ -35,5 +56,19 @@ window.onscroll = () => {
       element.style.cssText = "font-size: 12px;";
     });
    }
+   else {
+    document.querySelector('.nav').style.top = "-100px";
+    //logo styles
+    document.querySelector('.nav_logo').style.cssText = "width: 50px; padding: 10px 10px 0 0;";
+    document.querySelector('.nav_logo-syg').style.cssText = "width: 50px;";
+    document.querySelector('.nav_logo-txt').style.cssText = "width: 50px;";
+    //menu links styles
+    document.querySelectorAll('.nav_list-item').forEach(element => {
+      element.style.cssText = "padding: 12px 5px 20px 50px;";
+    });
+    document.querySelectorAll('.nav_list-item--link').forEach(element => {
+      element.style.cssText = "font-size: 10px;";
+    });
+  }
   prevScrollpos = currentScrollPos;
-}
+}*/
