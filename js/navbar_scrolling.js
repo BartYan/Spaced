6adbe585333a360b2e24006fb0c33d3b925b75d8
Bartosz Janusz <bartosz.yan@gmail.com>
2020-10-19 15:11:19 +0200

    nav test 2

--- js/navbar_scrolling.js
+++ js/navbar_scrolling.js
@@ -1,4 +1,75 @@
-let prevScrollpos = window.pageYOffset;
+
+(function(){
+
+  var doc = document.documentElement;
+  var w = window;
+
+  var prevScroll = w.scrollY || doc.scrollTop;
+  var curScroll;
+  var direction = 0;
+  var prevDirection = 0;
+
+  var header = document.querySelector('.nav');
+  //var logo = document.querySelector('.nav_logo');
+
+  var checkScroll = function() {
+
+    /*
+    ** Find the direction of scroll
+    ** 0 - initial, 1 - up, 2 - down
+    */
+
+    curScroll = w.scrollY || doc.scrollTop;
+    if (curScroll > prevScroll) { 
+      //scrolled up
+      direction = 2;
+    }
+    else if (curScroll < prevScroll) { 
+      //scrolled down
+      direction = 1;
+    }
+
+    if (direction !== prevDirection) {
+      toggleHeader(direction, curScroll);
+    }
+    
+    prevScroll = curScroll;
+  };
+
+  var toggleHeader = function(direction, curScroll) {
+    if (direction === 2 && curScroll > 52) { 
+      
+      //replace 52 with the height of your header in px
+
+      header.classList.add('hide');
+      prevDirection = direction;
+    }
+    else if (direction === 1) {
+      header.classList.remove('hide');
+      
+      prevDirection = direction;
+    }
+  };
+  
+  window.addEventListener('scroll', checkScroll);
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let prevScrollpos = window.pageYOffset;
 
 const menu = () => {
   let currentScrollPos = window.pageYOffset;
@@ -30,7 +101,7 @@ const menu = () => {
     });
     document.querySelectorAll('.nav_list-item--link').forEach(element => {
       element.style.cssText = "font-size: 12px;";
-    }); */
+    }); 
   prevScrollpos = currentScrollPos;
 
   
